d16107cdb0607d21e4aa2620cd5a03aa065e867d
Étienne <etienne.b.car@gmail.com>
2017-04-06 14:44:39 -0400

    Creation de fonctions qui permettent de faire des operations de base --HistoireToArbre assez self explanatory --getOccurence Renvoie le nombre d'occurence d'un mot dans un sous arbre quelconque

--- inf3105.cpp
+++ inf3105.cpp
@@ -14,37 +14,70 @@ using namespace std;
 
 
 vector< Histoire *> * lireDocuments(string a_nomFichier);
-
+int histoiresToArbre(vector<Histoire *> *histoires, ArbreMap<string, int> * foret);
+int getOccurences(string mot, int index, ArbreMap<string, int> * foret);
 
 int main() {
-    int nbHistoires = 0;
-    vector<Histoire *> * histoires = lireDocuments(string("listeDocument.xml"));
-    vector<string> mots;
-    vector<string>::const_iterator iter;
-    ArbreMap<string, int> *tabArbre = new ArbreMap<string, int>[histoires->size()];
-
-    for( Histoire * histoire : * histoires ) {
-        tabArbre[nbHistoires] = ArbreMap<string, int>();
-        for(iter = histoire->begin(); iter != histoire->end(); ++iter){
-            string temp = *iter;
-            temp[0] = tolower(temp[0]);
-            tabArbre[nbHistoires][temp]++;
-        }
-        ++nbHistoires;
-    }
-
-    for(int i = 0; i < nbHistoires; ++i){
-        ArbreMap<string, int> *temp = &tabArbre[i];
-        ArbreMap<string, int>::Iterateur it = temp->fin();
-        cout << it.cle() << endl;
-    }
-    delete [] tabArbre;
-	return 0;
+    vector<Histoire *> *histoires = lireDocuments(string("listeDocument.xml"));
+    ArbreMap<string, int> *foret = new ArbreMap<string, int>[histoires->size()];
+    int nbHistoires;
+    nbHistoires = histoiresToArbre(histoires, foret); // Permet de faire le transfert des mots des histoires dans leurs arbre respectif.
+                                                      // Renvoie le nombre d'histoire
+    cout << "Occurences de The :" << getOccurences("the", 2, foret) << endl;
+    delete [] foret;
+	  return 0;
 }
 
 /************************************************
  *                    FONCTIONS                 *
  ***********************************************/
+/**
+ * [histoiresToArbre Fonction permettant de faire le transfert d'un vecteur
+ * d'histoires a des arbres]
+ * @param  histoires [Vecteur d'histoire a transferer]
+ * @param  foret     [Arbre dans lequel on y mettra d'autres arbres contenant les mots]
+ * @return           [Le nombre d'histoires transferee]
+ */
+int histoiresToArbre(vector<Histoire *> *histoires, ArbreMap<string, int> * foret){
+
+      vector<string>::const_iterator iter;
+      int nbHistoires = 0;
+      for( Histoire * histoire : * histoires ) {
+        foret[nbHistoires] = ArbreMap<string, int>();
+        for(iter = histoire->begin(); iter != histoire->end(); ++iter){
+            string temp = *iter;
+            temp[0] = tolower(temp[0]);
+            foret[nbHistoires][temp]++;
+        }
+        ++nbHistoires;
+    }
+}
+/**
+ * [getOccurences Fonction retournant le nombre d'occurence d'un mot dans un sous arbre de foret]
+ * @param  mot   [Mot a rechercher]
+ * @param  index [Sous arbre a chercher]
+ * @param  foret [Ensemble d'arbre dans lequel l'on effectue la recherche binaire]
+ * @return       [Le nombre d'occurence du mot]
+ * ----------------------------------------------TO-DO --------------------------------------------
+ * je sais que la fonction cause probleme si l'on met un mot qu'il n'y a pas dans la foret. C'est donc
+ * une fonction a retravailler
+ */
+int getOccurences(string mot, int index, ArbreMap<string, int> * foret){
+     ArbreMap<string, int> *sousArbre = &foret[index];
+     ArbreMap<string, int>::Iterateur it = sousArbre->rechercher("the");
+     return it.valeur();
+}
+
+
+
+
+
+
+
+
+
+
+
 
 vector< Histoire *> * lireDocuments( string a_nomFichier) {
     vector<Histoire *> * histoires = new vector< Histoire * >();
